9853828647cf3c951192aac744eb5be80708d558
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-12-02 09:33:01 -0500

    Day 02

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,21 +1,44 @@
+fun <T> List<T>.dropAt(index: Int): List<T> = mapIndexedNotNull { i, item -> if (index != i) item else null }
+
 fun main() {
+    fun parse(input: List<String>): List<List<Int>> {
+        return input.map { it.split(' ') }.map { it.map { it.toInt() } }
+    }
+
+    fun List<Int>.isSafe(): Boolean {
+        val diffs = windowed(2).map { (a, b) -> b - a }
+        return diffs.all { it in 1..3 } || diffs.all { it >= -3 && it <= -1 }
+    }
+
+    fun List<Int>.isSafe2(): Boolean {
+        if (isSafe()) return true
+        indices.forEach { index ->
+            if (dropAt(index).isSafe()) {
+                return true
+            }
+        }
+        return false
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return parse(input).count { it.isSafe() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return parse(input).count { it.isSafe2() }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val testInput = """
+        7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9
+    """.trimIndent().split('\n')
+    check(part1(testInput) == 2)
+    val input = readInput("Day02")
     part1(input).println()
+    check(part2(testInput) == 4)
     part2(input).println()
 }
