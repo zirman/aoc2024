b4854601f5e888dac0505bbf0f1bdde122be237e
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-12-06 20:49:46 -0500

    Day 06 performance optimization

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,10 @@ sourceSets {
     }
 }
 
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.8")
+}
+
 tasks {
     wrapper {
         gradleVersion = "8.11"
--- src/Day06.kt
+++ src/Day06.kt
@@ -1,210 +1,244 @@
-private typealias Input6 = List<CharArray>
+import kotlin.system.measureTimeMillis
+
+private typealias Input6 = Triple<Pos, Set<Pos>, Size>
+
+data class Pos(val row: Int, val col: Int)
+data class Size(val width: Int, val height: Int)
+
+fun Pos.goWest(): Pos = copy(col = col - 1)
+fun Pos.goEast(): Pos = copy(col = col + 1)
+fun Pos.goNorth(): Pos = copy(row = row - 1)
+fun Pos.goSouth(): Pos = copy(row = row + 1)
+
+fun Pos.goWestIn(rect: Size): Pos? = if (col > 0) copy(col = col - 1) else null
+fun Pos.goEastIn(rect: Size): Pos? = if (col < rect.width - 1) copy(col = col + 1) else null
+fun Pos.goNorthIn(rect: Size): Pos? = if (row > 0) copy(row = row - 1) else null
+fun Pos.goSouthIn(rect: Size): Pos? = if (row < rect.height - 1) copy(row = row + 1) else null
 
 fun main() {
-    fun List<String>.parse(): Input6 = map { it.toCharArray() }
+    fun List<String>.parse(): Input6 {
+        var row = 0
+        var col = 0
+        var blockers = buildSet {
+            outer@ for (r in this@parse.indices) {
+                for (c in this@parse[r].indices) {
+                    when (this@parse[r][c]) {
+                        '^' -> {
+                            row = r
+                            col = c
+                        }
 
-    fun Input6.part1(): Int {
-        var row = indexOfFirst { it.contains('^') }
-        var col = this[row].indexOf('^')
-
-        outer@ while (true) {
-            while (true) {
-                this[row][col] = 'X'
-                if (row - 1 < 0) {
-                    break@outer
+                        '#' -> {
+                            add(Pos(r, c))
+                        }
+                    }
                 }
-                if (this[row - 1][col] == '#') {
-                    break
-                }
-                row--
-            }
-            while (true) {
-                this[row][col] = 'X'
-                if (col + 1 >= this[0].size) {
-                    break@outer
-                }
-                if (this[row][col + 1] == '#') {
-                    break
-                }
-
-                col++
-            }
-            while (true) {
-                this[row][col] = 'X'
-                if (row + 1 >= this.size) {
-                    break@outer
-                }
-                if (this[row + 1][col] == '#') {
-                    break
-                }
-                row++
-            }
-            while (true) {
-                this[row][col] = 'X'
-                if (col - 1 < 0) {
-                    break@outer
-                }
-                if (this[row][col - 1] == '#') {
-                    break
-                }
-                col--
             }
         }
-        return this.sumOf { it.count { ch -> ch == 'X' } }
+        return Triple(Pos(row, col), blockers, third = Size(width = this[0].length, height = this.size))
+    }
+
+    fun Input6.part1(): Int {
+        var pos = first
+        val blockerPos = second
+        val size = third
+        val visitedPos = mutableSetOf<Pos>()
+        outer@ while (true) {
+            while (true) {
+                visitedPos.add(pos)
+                if (blockerPos.contains(pos.goNorth())) break
+                pos = pos.goNorthIn(size) ?: break@outer
+            }
+            while (true) {
+                visitedPos.add(pos)
+                if (blockerPos.contains(pos.goEast())) break
+                pos = pos.goEastIn(size) ?: break@outer
+            }
+            while (true) {
+                visitedPos.add(pos)
+                if (blockerPos.contains(pos.goSouth())) break
+                pos = pos.goSouthIn(size) ?: break@outer
+            }
+            while (true) {
+                visitedPos.add(pos)
+                if (blockerPos.contains(pos.goWest())) break
+                pos = pos.goWestIn(size) ?: break@outer
+            }
+        }
+        return visitedPos.size
+    }
+
+    fun findLoop(
+        startPos: Pos,
+        startDir: Char,
+        blockPos: Set<Pos>,
+        newBlockPos: Pos,
+        visitedPosNorth: Set<Pos>,
+        visitedPosSouth: Set<Pos>,
+        visitedPosEast: Set<Pos>,
+        visitedPosWest: Set<Pos>,
+        size: Size,
+    ): Boolean {
+        var pos = startPos
+        var dir = startDir
+        val visitedPosNorth2 = mutableSetOf<Pos>()
+        val visitedPosSouth2 = mutableSetOf<Pos>()
+        val visitedPosEast2 = mutableSetOf<Pos>()
+        val visitedPosWest2 = mutableSetOf<Pos>()
+        while (true) {
+            if (dir == '^') {
+                while (true) {
+                    if (visitedPosNorth.contains(pos) || visitedPosNorth2.contains(pos)) {
+                        return true
+                    }
+                    visitedPosNorth2.add(pos)
+                    pos.goNorthIn(size) ?: return false
+                    val p = pos.goNorth()
+                    if (blockPos.contains(p) || p == newBlockPos) break
+                    pos = p
+                }
+                dir = '>'
+            }
+            if (dir == '>') {
+                while (true) {
+                    if (visitedPosEast.contains(pos) || visitedPosEast2.contains(pos)) {
+                        return true
+                    }
+                    visitedPosEast2.add(pos)
+                    pos.goEastIn(size) ?: return false
+                    val p = pos.goEast()
+                    if (blockPos.contains(p) || p == newBlockPos) break
+                    pos = p
+                }
+                dir = 'v'
+            }
+            if (dir == 'v') {
+                while (true) {
+                    if (visitedPosSouth.contains(pos) || visitedPosSouth2.contains(pos)) {
+                        return true
+                    }
+                    visitedPosSouth2.add(pos)
+                    pos.goSouthIn(size) ?: return false
+                    val p = pos.goSouth()
+                    if (blockPos.contains(p) || p == newBlockPos) break
+                    pos = p
+                }
+                dir = '<'
+            }
+            if (dir == '<') {
+                while (true) {
+                    if (visitedPosWest.contains(pos) || visitedPosWest2.contains(pos)) {
+                        return true
+                    }
+                    visitedPosWest2.add(pos)
+                    pos.goWestIn(size) ?: return false
+                    val p = pos.goWest()
+                    if (blockPos.contains(p) || p == newBlockPos) break
+                    pos = p
+                }
+                dir = '^'
+            }
+        }
+        return false
     }
 
     fun Input6.part2(): Int {
-        var row = indexOfFirst { it.contains('^') }
-        var col = this[row].indexOf('^')
-        val visited = mutableSetOf<Triple<Int, Int, Char>>()
-
-        fun Input6.findLoop(row: Int, col: Int, dir: Char): Boolean {
-            var row = row
-            var col = col
-            var dir = dir
-            val visited = visited.toMutableSet()
-            while (true) {
-                if (dir == '^') {
-                    while (true) {
-                        if (visited.contains(Triple(row, col, dir))) {
-                            return true
-                        }
-                        visited.add(Triple(row, col, dir))
-                        if (row - 1 < 0) {
-                            return false
-                        }
-                        if (this[row - 1][col] == '#') {
-                            break
-                        }
-                        row--
-                    }
-                    dir = '>'
-                }
-                if (dir == '>') {
-                    while (true) {
-                        if (visited.contains(Triple(row, col, dir))) {
-                            return true
-                        }
-                        visited.add(Triple(row, col, dir))
-                        if (col + 1 >= this[0].size) {
-                            return false
-                        }
-                        if (this[row][col + 1] == '#') {
-                            break
-                        }
-                        col++
-                    }
-                    dir = 'v'
-                }
-                if (dir == 'v') {
-                    while (true) {
-                        if (visited.contains(Triple(row, col, dir))) {
-                            return true
-                        }
-                        visited.add(Triple(row, col, dir))
-                        if (row + 1 >= this.size) {
-                            return false
-                        }
-                        if (this[row + 1][col] == '#') {
-                            break
-                        }
-                        row++
-                    }
-                    dir = '<'
-                }
-                if (dir == '<') {
-                    while (true) {
-                        if (visited.contains(Triple(row, col, dir))) {
-                            return true
-                        }
-                        visited.add(Triple(row, col, dir))
-                        if (col - 1 < 0) {
-                            return false
-                        }
-                        if (this[row][col - 1] == '#') {
-                            break
-                        }
-                        col--
-                    }
-                    dir = '^'
-                }
-            }
-            return false
-        }
-
-        var blocks = mutableSetOf<Pair<Int, Int>>()
+        var pos = first
+        val blockPos = second
+        val size = third
+        val visitedPosNorth = mutableSetOf<Pos>()
+        val visitedPosEast = mutableSetOf<Pos>()
+        val visitedPosWest = mutableSetOf<Pos>()
+        val visitedPosSouth = mutableSetOf<Pos>()
+        val visitedPos = listOf(visitedPosNorth, visitedPosEast, visitedPosWest, visitedPosSouth)
+        val newBlockPos = mutableSetOf<Pos>()
         outer@ while (true) {
             while (true) {
-                this[row][col] = 'X'
-                visited.add(Triple(row, col, '^'))
-                if (row - 1 < 0) {
-                    break@outer
-                }
-                if (this[row - 1][col] == '#') {
-                    break
-                }
-                if (this[row - 1][col] != 'X' && blocks.contains(Pair(row - 1, col))
-                        .not() && this.map { chars -> chars.clone() }.apply { this[row - 1][col] = '#' }
-                        .findLoop(row, col, '>')
+                visitedPosNorth.add(pos)
+                val newBlock = pos.goNorth()
+                if (blockPos.contains(newBlock)) break
+                if (visitedPos.none { it.contains(newBlock) } &&
+                    findLoop(
+                        startPos = pos,
+                        startDir = '>',
+                        blockPos = blockPos,
+                        newBlockPos = newBlock,
+                        visitedPosNorth = visitedPosNorth,
+                        visitedPosSouth = visitedPosSouth,
+                        visitedPosEast = visitedPosEast,
+                        visitedPosWest = visitedPosWest,
+                        size = size,
+                    )
                 ) {
-
-                    blocks.add(Pair(row - 1, col))
+                    newBlockPos.add(newBlock)
                 }
-                row--
+                pos = pos.goNorthIn(size) ?: break@outer
             }
             while (true) {
-                this[row][col] = 'X'
-                visited.add(Triple(row, col, '>'))
-                if (col + 1 >= this[0].size) {
-                    break@outer
-                }
-                if (this[row][col + 1] == '#') {
-                    break
-                }
-                if (this[row][col + 1] != 'X' && blocks.contains(Pair(row, col + 1))
-                        .not() && this.map { chars -> chars.clone() }.apply { this[row][col + 1] = '#' }
-                        .findLoop(row, col, 'v')
+                visitedPosEast.add(pos)
+                val newBlock = pos.goEast()
+                if (blockPos.contains(newBlock)) break
+                if (visitedPos.none { it.contains(newBlock) } &&
+                    findLoop(
+                        startPos = pos,
+                        startDir = 'v',
+                        blockPos = blockPos,
+                        newBlockPos = newBlock,
+                        visitedPosNorth = visitedPosNorth,
+                        visitedPosSouth = visitedPosSouth,
+                        visitedPosEast = visitedPosEast,
+                        visitedPosWest = visitedPosWest,
+                        size = size,
+                    )
                 ) {
-                    blocks.add(Pair(row, col + 1))
+                    newBlockPos.add(newBlock)
                 }
-                col++
+                pos = pos.goEastIn(size) ?: break@outer
             }
             while (true) {
-                this[row][col] = 'X'
-                visited.add(Triple(row, col, 'v'))
-                if (row + 1 >= this.size) {
-                    break@outer
-                }
-                if (this[row + 1][col] == '#') {
-                    break
-                }
-                if (this[row + 1][col] != 'X' && blocks.contains(Pair(row + 1, col))
-                        .not() && this.map { chars -> chars.clone() }.apply { this[row + 1][col] = '#' }
-                        .findLoop(row, col, '<')
+                visitedPosSouth.add(pos)
+                val newBlock = pos.goSouth()
+                if (blockPos.contains(newBlock)) break
+                if (visitedPos.none { it.contains(newBlock) } &&
+                    findLoop(
+                        startPos = pos,
+                        startDir = '<',
+                        blockPos = blockPos,
+                        newBlockPos = newBlock,
+                        visitedPosNorth = visitedPosNorth,
+                        visitedPosSouth = visitedPosSouth,
+                        visitedPosEast = visitedPosEast,
+                        visitedPosWest = visitedPosWest,
+                        size = size,
+                    )
                 ) {
-                    blocks.add(Pair(row + 1, col))
+                    newBlockPos.add(newBlock)
                 }
-                row++
+                pos = pos.goSouthIn(size) ?: break@outer
             }
             while (true) {
-                this[row][col] = 'X'
-                visited.add(Triple(row, col, '<'))
-                if (col - 1 < 0) {
-                    break@outer
-                }
-                if (this[row][col - 1] == '#') {
-                    break
-                }
-                if (this[row][col - 1] != 'X' && blocks.contains(Pair(row, col - 1))
-                        .not() && this.map { chars -> chars.clone() }.apply { this[row][col - 1] = '#' }
-                        .findLoop(row, col, '^')
+                visitedPosWest.add(pos)
+                val newBlock = pos.goWest()
+                if (blockPos.contains(newBlock)) break
+                if (visitedPos.none { it.contains(newBlock) } &&
+                    findLoop(
+                        startPos = pos,
+                        startDir = '^',
+                        blockPos = blockPos,
+                        newBlockPos = newBlock,
+                        visitedPosNorth = visitedPosNorth,
+                        visitedPosSouth = visitedPosSouth,
+                        visitedPosEast = visitedPosEast,
+                        visitedPosWest = visitedPosWest,
+                        size = size,
+                    )
                 ) {
-                    blocks.add(Pair(row, col - 1))
+                    newBlockPos.add(newBlock)
                 }
-                col--
+                pos = pos.goWestIn(size) ?: break@outer
             }
         }
-        return blocks.size
+        return newBlockPos.size
     }
 
     val testInput = """
@@ -218,10 +252,10 @@ fun main() {
         ........#.
         #.........
         ......#...
-    """.trimIndent().split('\n')
-    check(testInput.parse().part1() == 41)
-    val input = readInput("Day06")
-    input.parse().part1().println()
-    check(testInput.parse().part2() == 6)
-    input.parse().part2().println()
+    """.trimIndent().split('\n').parse()
+    check(testInput.part1() == 41)
+    val input = readInput("Day06").parse()
+    measureTimeMillis { input.part1().println() }.also { println("time: $it") }
+    check(testInput.part2().also { println(it) } == 6)
+    measureTimeMillis { input.part2().println() }.also { println("time: $it") }
 }
