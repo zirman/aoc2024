f85a7703c5709581427a051b25536b5fb10bd327
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-12-10 00:28:54 -0500

    Day 10

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,23 +1,71 @@
-private typealias Input10 = List<String>
+private typealias Input10 = Pair<List<List<Int>>, Size>
 
 fun main() {
     fun List<String>.parse(): Input10 {
-        return this
+        return Pair(map { it.map { it.digitToInt() } }, toSize())
     }
 
     fun Input10.part1(): Int {
-        TODO()
+        val (arr, size) = this
+        fun Pos.score(): Set<Pos> {
+            val height = arr[row][col]
+            if (height == 9) {
+                return setOf(this)
+            }
+            val heightPlus = height + 1
+            val next = setOf(
+                goNorth(),
+                goSouth(),
+                goWest(),
+                goEast(),
+            ).filter { it in size && arr[it.row][it.col] == heightPlus }
+            return next.flatMap { it.score() }.toSet()
+        }
+
+        return arr.indices.flatMap { row ->
+            arr[row].indices.mapNotNull { col ->
+                if (arr[row][col] == 0) Pos(row, col) else null
+            }
+        }.sumOf { it.score().size }
     }
 
     fun Input10.part2(): Int {
-        TODO()
+        val (arr, size) = this
+        fun Pos.score(): Int {
+            val height = arr[row][col]
+            if (height == 9) {
+                return 1
+            }
+            val heightPlus = height + 1
+            val next = setOf(
+                goNorth(),
+                goSouth(),
+                goWest(),
+                goEast(),
+            ).filter { it in size && arr[it.row][it.col] == heightPlus }
+            return next.sumOf { it.score() }
+        }
+
+        return arr.indices.flatMap { row ->
+            arr[row].indices.mapNotNull { col ->
+                if (arr[row][col] == 0) Pos(row, col) else null
+            }
+        }.sumOf { it.score() }
     }
 
     val testInput = """
+        89010123
+        78121874
+        87430965
+        96549874
+        45678903
+        32019012
+        01329801
+        10456732
     """.trimIndent().split('\n').parse()
-    check(testInput.part1() == TODO())
+    check(testInput.part1() == 36)
     val input = readInput("Day10").parse()
-    input.part1().println()
-//    check(testInput.part2() == 4)
-//    input.part2().println()
+    printlnMeasureTimeMillis { input.part1().println() }
+    check(testInput.part2() == 81)
+    input.part2().println()
 }
