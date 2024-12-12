64e29abea863a22234630d2ecf1e9e199fd5f58f
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-12-11 22:56:52 -0500

    Day 11 cleanup

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,28 +1,7 @@
 private typealias Input11 = String
 
 fun main() {
-    fun Input11.part1(): Int {
-        val regex = """^0*([1-9][0-9]*|0)$""".toRegex()
-        var stones = split(' ')
-        repeat(25) {
-            stones = buildList {
-                stones.forEach { stone ->
-                    if (stone == "0") {
-                        add("1")
-                    } else if (stone.length % 2 == 0) {
-                        add(stone.substring(0..<stone.length / 2))
-                        val (a) = regex.matchEntire(stone.substring(stone.length / 2))!!.destructured
-                        add(a)
-                    } else {
-                        add((stone.toLong() * 2024).toString())
-                    }
-                }
-            }
-        }
-        return stones.size
-    }
-
-    fun Input11.part2(): Long {
+    fun Input11.blinkTimes(blinks: Int): Long {
         val regex = """^0*([1-9][0-9]*|0)$""".toRegex()
         val memo = mutableMapOf<Pair<String, Int>, Long>()
         val recur = DeepRecursiveFunction<Pair<String, Int>, Long> { stoneBlinks ->
@@ -41,11 +20,13 @@ fun main() {
                 }
             }
         }
-        return split(' ').sumOf { recur(Pair(it, 75)) }
+        return split(' ').sumOf { recur(Pair(it, blinks)) }
     }
 
+    fun Input11.part1(): Long = blinkTimes(25)
+    fun Input11.part2(): Long = blinkTimes(75)
     val input = "64599 31 674832 2659361 1 0 8867 321"
-    check("125 17".part1() == 55312)
+    check("125 17".part1() == 55312L)
     printlnMeasureTimeMillis { input.part1().println() }
     printlnMeasureTimeMillis { input.part2().println() }
 }
