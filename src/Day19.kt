77e7a87a74fb53fd57c9e1b215666fe50d4dbb10
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-12-19 00:45:40 -0500

    Day 19

--- src/Day19.kt
+++ src/Day19.kt
@@ -1,23 +1,73 @@
-private typealias Input19 = List<String>
+private typealias Input19 = Pair<List<String>, List<String>>
 
 fun main() {
     fun List<String>.parse(): Input19 {
-        return this
+        val (towels, patterns) = joinToString("\n").split("\n\n")
+        return Pair(
+            towels.split(", ").sortedDescending(),
+            patterns.split('\n'),
+        )
     }
 
     fun Input19.part1(): Int {
-        TODO()
+        val (towels, patterns) = this
+        val memo = mutableMapOf<String, Boolean>()
+        return patterns.filter { pattern ->
+            fun recur(pattern: String): Boolean {
+                return memo.getOrPut(pattern) {
+                    if (pattern.isEmpty()) {
+                        true
+                    } else {
+                        towels.any { towel ->
+                            if (pattern.startsWith(towel)) {
+                                recur(pattern.substring(startIndex = towel.length))
+                            } else {
+                                false
+                            }
+                        }
+                    }
+                }
+            }
+            recur(pattern)
+        }.count()
     }
 
-    fun Input19.part2(): Int {
-        TODO()
+    fun Input19.part2(): Long {
+        val (towels, patterns) = this
+        val memo = mutableMapOf<String, Long>()
+        return patterns.sumOf { pattern ->
+            fun recur(pattern: String): Long = memo.getOrPut(pattern) {
+                if (pattern.isEmpty()) {
+                    1
+                } else {
+                    towels.sumOf { towel ->
+                        if (pattern.startsWith(towel)) {
+                            recur(pattern.substring(startIndex = towel.length))
+                        } else {
+                            0
+                        }
+                    }
+                }
+            }
+            recur(pattern)
+        }
     }
 
     val testInput = """
+        r, wr, b, g, bwu, rb, gb, br
+
+        brwrr
+        bggr
+        gbbr
+        rrbgbr
+        ubwu
+        bwurrg
+        brgr
+        bbrgwb
     """.trimIndent().split('\n').parse()
-    check(testInput.part1() == TODO())
+    check(testInput.part1() == 6)
     val input = readInput("Day19").parse()
-    input.part1().println()
-//    check(testInput.part2() == 4)
-//    input.part2().println()
+    printlnMeasureTimeMillis { println("part1: ${input.part1()}") }
+    check(testInput.part2() == 16L)
+    printlnMeasureTimeMillis { println("part2: ${input.part2()}") }
 }
