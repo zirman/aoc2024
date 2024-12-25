ca3a18088a0326ae920ffa4c0141fa9d2d691a0c
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-12-25 09:39:01 -0500

    Day 25

--- src/Day25.kt
+++ src/Day25.kt
@@ -1,12 +1,41 @@
-private typealias Input25 = List<String>
+private typealias Input25 = Pair<List<List<Int>>, List<List<Int>>>
 
 fun main() {
     fun List<String>.parse(): Input25 {
-        return this
+        val locks = mutableListOf<List<Int>>()
+        val keys = mutableListOf<List<Int>>()
+        joinToString("\n").split("\n\n").map {
+            val t = it.split('\n').map { it.toList() }.transpose()
+            if (t.first().first() == '#') {
+                // lock
+                locks.add(
+                    t.map {
+                        it.takeWhile { it == '#' }.count() - 1
+                    },
+                )
+            } else {
+                // key
+                keys.add(
+                    t.map {
+                        it.asReversed().takeWhile { it == '#' }.count() - 1
+                    },
+                )
+            }
+        }
+        return Pair(locks, keys)
     }
 
     fun Input25.part1(): Int {
-        TODO()
+        val (locks, keys) = this
+        return locks.flatMap { lock ->
+            keys.map { key ->
+                if (lock.indices.all { lock[it] + key[it] <= 5 }) {
+                    1
+                } else {
+                    0
+                }
+            }
+        }.sum()
     }
 
     fun Input25.part2(): Int {
@@ -14,10 +43,46 @@ fun main() {
     }
 
     val testInput = """
+        #####
+        .####
+        .####
+        .####
+        .#.#.
+        .#...
+        .....
+
+        #####
+        ##.##
+        .#.##
+        ...##
+        ...#.
+        ...#.
+        .....
+
+        .....
+        #....
+        #....
+        #...#
+        #.#.#
+        #.###
+        #####
+
+        .....
+        .....
+        #.#..
+        ###..
+        ###.#
+        ###.#
+        #####
+
+        .....
+        .....
+        .....
+        #....
+        #.#..
+        #.#.#
+        #####
     """.trimIndent().split('\n').parse()
-    check(testInput.part1() == TODO())
     val input = readInput("Day25").parse()
-    input.part1().println()
-//    check(testInput.part2() == 4)
-//    input.part2().println()
+    printlnMeasureTimeMillis { println("part1: ${input.part1()}") }
 }
